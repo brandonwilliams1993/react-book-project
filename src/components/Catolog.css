74374ec9c630702846721672c39a1f0fefa08572
Brandon Cruz <168683452+brandonwilliams1993@users.noreply.github.com>
2024-08-20 09:07:36 -0700

    Create Catolog.css

---
+++ src/components/Catolog.css
@@ -0,0 +1,15 @@
+.catalog-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .catalog-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 20px;
+    justify-content: center;
+    width: 100%;
+    max-width: 1200px; 
+    padding: 20px;
+  }
