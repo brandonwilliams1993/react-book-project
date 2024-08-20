78eab9dee6187d6044170107d52a0d53538eb426
Brandon Cruz <168683452+brandonwilliams1993@users.noreply.github.com>
2024-08-20 09:07:51 -0700

    Update Favorite.js

---
+++ src/components/Favorite.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Favorites = () => {
+    return (
+        <div>
+            <h1>Favorites</h1>
+            <p>Here are your favorite items.</p>
+        </div>
+    );
+};
+
+export default Favorites;
